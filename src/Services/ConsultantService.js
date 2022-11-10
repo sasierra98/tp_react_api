b924413485c53a56b2ec11c3f728377b79d5153a
Jose Andres Sierra Arrieta <joseandresmayo@gmail.com>
2022-11-10 01:05:46 -0500

    Default axios instance added

--- src/Services/ConsultantService.js
+++ src/Services/ConsultantService.js
@@ -1,4 +1,9 @@
-export const baseURL = "http://localhost:8000/api/v1";
-export const headers = {
-  "Content-type": "application/json",
-};
+import axios from 'axios';
+
+export default axios.create({
+    baseURL: "http://127.0.0.1:8000/api/v1",
+    headers: {
+        'Accept':'application/json',
+        'Content-Type':'application/json',
+    }
+})
